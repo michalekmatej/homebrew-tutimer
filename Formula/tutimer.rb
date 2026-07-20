047d37ee1140497411b929a2d6e263426af5b2fa
Matěj Michálek <42139665+michalekmatej@users.noreply.github.com>
2026-07-20 15:50:16 +0200

    tutimer 1.1.0

--- Formula/tutimer.rb
+++ Formula/tutimer.rb
@@ -1,12 +1,12 @@
 class Tutimer < Formula
   desc "Terminal speedcubing timer"
   homepage "https://github.com/michalekmatej/TUtimer"
-  version "1.0.0"
+  version "1.1.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/michalekmatej/homebrew-tutimer/releases/download/v1.0.0/tutimer-1.0.0-aarch64-apple-darwin.tar.gz"
-      sha256 "ab2cc33e28a80db93d62220335451b5a37938c2b3d55765a2dff066f8aa1d2d9"
+      url "https://github.com/michalekmatej/homebrew-tutimer/releases/download/v1.1.0/tutimer-1.1.0-aarch64-apple-darwin.tar.gz"
+      sha256 "8b3520017768265856eb8e3de9cde42fd38411dd34a9a0fc5068dadb49089cad"
     else
       odie "tutimer currently ships Homebrew binaries for Apple Silicon only."
     end
